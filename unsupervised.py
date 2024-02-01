2ee250351ceca6ebaf029abe174f60dd80c01104
narin.dap <narin.dap@ascendcorp.com>
2024-02-01 15:02:06 +0700

    unsup

--- unsupervised.py
+++ unsupervised.py
@@ -21,13 +21,13 @@ def similar_word(input_text, log_data):
     #with open(input_file_path, 'r', encoding='utf-8') as input_file:
         #processed_text = wordcut(input_file)
     processed_text = wordcut([input_text])
-    words_per_line = 1
+    words_per_line = 2
     model = train_fasttext_model(log_data)
     words = processed_text.split()
     chunks = [words[i:i + words_per_line] for i in range(0, len(words), words_per_line)]
     for chunk in chunks:
         chunk_sentence = ' '.join(chunk)
-        similar_words = model.get_nearest_neighbors(chunk_sentence, k=3)
+        similar_words = model.get_nearest_neighbors(chunk_sentence, k=1)
         for word, similarity in similar_words:
             if float(word) > 0.5:
                 print(f"[ {chunk_sentence} ] \n########## {similarity} : {word} ##########")
@@ -37,10 +37,10 @@ if __name__ == "__main__":
 
     model_settings = {
     'model': 'skipgram',
-    'minCount': 1,
+    'minCount': 1, #จำนวนต่ำสุดของครั้งที่คำต้องปรากฏในข้อมูลฝึกอบรม
     'epoch': 450,
     'word_ngrams': 1,
-    'dim': 100
+    'dim': 300
 }
     print("Enter your sentence:")
     text_input = input()
