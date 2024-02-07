84556d1acce31892936a0b3eb3925e7a678c597e
narin.dap <narin.dap@ascendcorp.com>
2024-02-08 00:24:42 +0700

    commit

--- wordcut_utils.py
+++ wordcut_utils.py
@@ -5,30 +5,31 @@ from nltk import PorterStemmer
 nlp = spacy.load("en_core_web_sm")
 stemmer = PorterStemmer()
 
-def wordcut_input(input_text):
-    sentences = []
+def clean_data(line):
+    line = re.sub(r'[^a-zA-Z0-9 ]', '', line).strip()
+    doc = nlp(line)
+    english_words = [stemmer.stem(token.lemma_.lower()) for token in doc]
+    return english_words
+
+def preprocess_input(input_text):
+    preprocessed_sentences = []
 
     for line in input_text:
-        line = re.sub(r'[^a-zA-Z0-9 ]', '', line).strip()
-        doc = nlp(line)
-        english_words = [stemmer.stem(token.lemma_.lower()) for token in doc]
+        english_words = clean_data(line)
         sentence = ' '.join(english_words)
-        sentences.append(sentence)
-    return ''.join(sentences)
+        preprocessed_sentences.append(sentence)
+    return ''.join(preprocessed_sentences)
     
-def wordcut_unsupervised(input_text):
-    sentences = []
+def preprocess_unsupervised(input_text):
+    preprocessed_sentences = []
 
     for line in input_text:
-        line = re.sub(r'[^a-zA-Z0-9 ]', '', line).strip()
-        doc = nlp(line)
-        english_words = [stemmer.stem(token.lemma_.lower()) for token in doc]
+        english_words = clean_data(line)
         sentence = ''.join(english_words)
-        sentences.append(sentence)
-    print(sentence)
-    return ' '.join(sentences)
+        preprocessed_sentences.append(sentence)
+    return ' '.join(preprocessed_sentences)
 
-def process_text(text):
+def preprocess_text(text):
     doc = nlp(text)
     stemmed_and_lemmatized = [stemmer.stem(token.lemma_.lower()) for token in doc]
     return ' '.join(stemmed_and_lemmatized)
